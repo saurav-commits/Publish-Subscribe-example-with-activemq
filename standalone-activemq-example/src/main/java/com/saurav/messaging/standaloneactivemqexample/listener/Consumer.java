e47eddcc07e20ba333376e1aa3bdf9053cad6949
Saurav <sauravsky55@yahoo.com>
2021-09-09 12:19:51 +0530

    Initial Commit

---
+++ standalone-activemq-example/src/main/java/com/saurav/messaging/standaloneactivemqexample/listener/Consumer.java
@@ -0,0 +1,15 @@
+package com.saurav.messaging.standaloneactivemqexample.listener;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "standalone.queue")
+    public void consume(String message) {
+        System.out.println("Recieved Message: " + message);
+    }
+}
